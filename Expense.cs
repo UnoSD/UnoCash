e00a80023ef541ebbfa25ad472ffd2ee9ebff80b
UnoSD <unosd.dev@gmail.com>
2019-07-03 18:24:19 +0000

    Add all properties

--- Expense.cs
+++ Expense.cs
@@ -7,5 +7,8 @@ namespace UnoCash
         public decimal Amount { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        public string Account { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
     }
 }
