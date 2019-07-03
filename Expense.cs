59dc51d4339f41dbf0899a44cdf8665ffd174579
UnoSD <unosd.dev@gmail.com>
2019-07-03 17:52:48 +0000

    Added date and fixed amount

--- Expense.cs
+++ Expense.cs
@@ -1,8 +1,11 @@
-﻿namespace UnoCash
+﻿using System;
+
+namespace UnoCash
 {
     public class Expense
     {
         public decimal Amount { get; set; }
         public string Description { get; set; }
+        public DateTime Date { get; set; }
     }
 }
