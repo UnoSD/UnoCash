785122cbdbe583f2cfc7c803a0acd1eb6d5df792
UnoSD <unosd.dev@gmail.com>
2019-07-12 18:53:23 +0000

    Making fold functions more functional without iterator

--- UnoCash.Core/AsyncLinq.cs
+++ UnoCash.Core/AsyncLinq.cs
@@ -8,7 +8,7 @@ namespace UnoCash.Core
     static class AsyncLinq
     {
         internal static Task<IEnumerable<T>> WhereAsync<T>(this Task<IEnumerable<T>> task,
-                                                           Func<T, bool> predicate) => 
+                                                           Func<T, bool> predicate) =>
             task.Map(collection => collection.Where(predicate));
 
         internal static Task<IEnumerable<TOut>> SelectAsync<TIn, TOut>(this Task<IEnumerable<TIn>> functor,
@@ -16,34 +16,30 @@ namespace UnoCash.Core
             functor.Map(collection => collection.Select(func));
 
         internal static IEnumerable<T> Unfold<TState, T>(this TState state,
-                                                         Func<TState, (T, TState)> generator, 
-                                                         Func<TState, bool> stopCondition)
-        {
-            if (stopCondition(state))
-                yield break;
-            
-            var (value, newState) = generator(state);
+                                                         Func<TState, (T item, TState state)> generator,
+                                                         Func<TState, bool> stopCondition) =>
+            stopCondition(state) ?
+            Enumerable.Empty<T>() :
+            generator(state).Unfold(generator, stopCondition);
 
-            yield return value;
-
-            foreach (var nextValue in Unfold(newState, generator, stopCondition))
-                yield return nextValue;
-        }
+        internal static IEnumerable<T> Unfold<TState, T>(this (T item, TState state) generated,
+                                                         Func<TState, (T, TState)> generator,
+                                                         Func<TState, bool> stopCondition) =>
+            stopCondition(generated.state) ?
+            new [] { generated.item } :
+            Unfold(generated.state, generator, stopCondition).Cons(generated.item);
 
         internal static IEnumerable<T> Unfold<TState, T>(this TState state,
-                                                         Func<TState, (T, TState)?> generator)
-        {
-            var nullable = generator(state);
+                                                         Func<TState, (T, TState)?> generator) =>
+            generator(state).Unfold(generator);
 
-            if (!nullable.HasValue)
-                yield break;
-            
-            var (value, newState) = nullable.Value;
+        static IEnumerable<T> Unfold<TState, T>(this (T item, TState state)? nullable,
+                                                Func<TState, (T, TState)?> generator) =>
+            nullable.HasValue ?
+            Unfold(nullable.Value.state, generator).Cons(nullable.Value.item) :
+            Enumerable.Empty<T>();
 
-            yield return value;
-
-            foreach (var nextValue in Unfold(newState, generator))
-                yield return nextValue;
-        }
+        internal static IEnumerable<T> Cons<T>(this IEnumerable<T> source, T item) =>
+            new[] { item }.Concat(source);
     }
 }
