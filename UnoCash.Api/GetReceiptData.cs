1847e5163b82bb9c21abeb75389ef4b92f3f0294
UnoSD <unosd.dev@gmail.com>
2019-07-13 11:33:56 +0000

    Remove unused imports

--- UnoCash.Api/GetReceiptData.cs
+++ UnoCash.Api/GetReceiptData.cs
@@ -1,12 +1,10 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using UnoCash.Core;
 
 namespace UnoCash.Api
 {
