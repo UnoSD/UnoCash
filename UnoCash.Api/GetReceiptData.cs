8cede4bbfdb3bf5ce308f322238f5caca4380d4b
UnoSD <unosd.dev@gmail.com>
2019-07-13 11:17:08 +0000

    Upload receipt to blob storage and get results from analysis function

---
+++ UnoCash.Api/GetReceiptData.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using UnoCash.Core;
+
+namespace UnoCash.Api
+{
+    public static class GetReceiptData
+    {
+        [FunctionName("GetReceiptData")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+            HttpRequest req,
+            ILogger log)
+        {
+            var blob = req.Query["blobName"];
+
+            log.LogWarning($"Processing the receipt for {blob}");
+
+            return new OkObjectResult(new
+            {
+                Payee = "Tesco",
+                Date = DateTime.Today.AddDays(-400),
+                Method = "Cash",
+                Amount = 12.12M
+            });
+        }
+    }
+}
