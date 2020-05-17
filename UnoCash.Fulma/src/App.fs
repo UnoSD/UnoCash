67e20122b82f33cf0576a7655fd9268469761fa0
UnoSD <unosd.dev@gmail.com>
2020-05-17 16:33:43 +0000

    Remove redundant code

--- UnoCash.Fulma/src/App.fs
+++ UnoCash.Fulma/src/App.fs
@@ -5,8 +5,9 @@ open Fable.React
 open Fable.React.Props
 open Fulma
 open Fable.FontAwesome
-open Fulma.Elmish
 open System
+open Elmish.Debug
+open Elmish.HMR
 
 type Tab =
     | AddExpense
@@ -15,26 +16,18 @@ type Tab =
     | About
 
 type Model =
-    { 
-        Value : string
+    {
         CurrentTab : Tab
-        DatePickerState : DatePicker.Types.State
-        CurrentDate : DateTime option
         Amount : decimal
     }
 
 type Msg =
-    | ChangeValue of string
     | ChangeToTab of Tab
-    | DatePickerChanged of DatePicker.Types.State * (DateTime option)
-    | AmountChanged of string
+    | ChangeAmount of string
 
 let init _ =
     {
-        Value = ""
         CurrentTab = AddExpense
-        DatePickerState = { DatePicker.Types.defaultState with AutoClose = true; ShowDeleteButton = true }
-        CurrentDate = None
         Amount = 0m
     }, Cmd.none
 
@@ -45,66 +38,12 @@ let sanitize value =
 
 let private update msg model =
     match msg with
-    | ChangeValue newValue ->
-        { model with Value = newValue }, Cmd.none
-    | ChangeToTab newTab ->
-        { model with CurrentTab = newTab }, Cmd.none
-    | DatePickerChanged (newState, date) ->
-        { model with DatePickerState = newState
-                     CurrentDate = date }, Cmd.none
-    | AmountChanged newValue -> { model with Amount = sanitize newValue }, Cmd.none
-
-let pickerConfig : DatePicker.Types.Config<Msg> =
-    DatePicker.Types.defaultConfig DatePickerChanged
-
-let private view model dispatch =
-    Hero.hero [ Hero.IsFullHeight ]
-        [ Hero.body [ ]
-            [ Container.container [ ]
-                [ Columns.columns [ Columns.CustomClass "has-text-centered" ]
-                    [ Column.column [ Column.Width(Screen.All, Column.IsOneThird)
-                                      Column.Offset(Screen.All, Column.IsOneThird) ]
-                        [ Image.image [ Image.Is128x128
-                                        Image.Props [ Style [ Margin "auto"] ] ]
-                            [ img [ Src "assets/fulma_logo.svg" ] ]
-                          Field.div [ ]
-                            [ Label.label [ ]
-                                [ str "Enter your name, dumbass!!!!" ]
-                              Control.div [ ]
-                                [ Input.text [ Input.OnChange (fun ev -> dispatch (ChangeValue ev.Value))
-                                               Input.Value model.Value
-                                               Input.Props [ AutoFocus true ] ] ] ]
-                          Content.content [ ]
-                            [ str "Hello, "
-                              str model.Value
-                              str " "
-                              Icon.icon [ ]
-                                [ Fa.i [ Fa.Regular.Smile ]
-                                    [ ] ] ] ] ] ] ] ]
+    | ChangeToTab newTab    -> { model with CurrentTab = newTab }, Cmd.none
+    | ChangeAmount newValue -> { model with Amount = sanitize newValue }, Cmd.none
 
 let tab model dispatch tabType title =
     Tabs.tab [ Tabs.Tab.IsActive (model.CurrentTab = tabType) ]
              [ a [ OnClick (fun _ -> ChangeToTab tabType |> dispatch) ] [ str title ] ]
-
-//Field.div [ ]
-//          [ Label.label [ ] [ str "Date" ]
-//            Control.div [ ] [ DatePicker.View.root pickerConfig model.DatePickerState model.CurrentDate dispatch ] ]
-
-let tags =
-    Field.div [ Field.IsGroupedMultiline ]
-              [ Control.div [ ]
-                  [ Tag.list [ Tag.List.HasAddons ]
-                      [ Tag.tag [ Tag.Color IsDanger ] [ str "Maxime Mangel" ]
-                        Tag.delete [ ] [ ] ] ]
-                Control.div [ ]
-                  [ Tag.list [ Tag.List.HasAddons ]
-                      [ Tag.tag [ Tag.Color IsLight ] [ str "28 Februari 1982" ]
-                        Tag.tag [ Tag.Color IsInfo ]
-                          [ Icon.icon [ ] [ i [ ClassName "fas fa-birthday-cake" ] [ ] ] ] ] ]
-                Control.div [ ]
-                  [ Tag.list [ Tag.List.HasAddons ]
-                      [ Tag.tag [ Tag.Color IsLight ] [ str "37 years"  ]
-                        Tag.tag [ Tag.Color IsInfo ] [ str "age" ] ] ] ]
 
 let addExpensePage model dispatch =
     form [ ]
@@ -141,17 +80,16 @@ let addExpensePage model dispatch =
                        Control.div [ Control.HasIconLeft ]
                                    [ Input.number [ Input.DefaultValue "0.00"
                                                     Input.Props [ Props.Step "0.01" ]
-                                                    Input.OnChange (fun ev -> AmountChanged ev.Value |> dispatch)
+                                                    Input.OnChange (fun ev -> ChangeAmount ev.Value |> dispatch)
                                                     Input.Value (string model.Amount) ]
                                      Icon.icon [ Icon.Size IsSmall
                                                  Icon.IsLeft ]
                                                [ Fa.i [ Fa.Solid.DollarSign ] [ ] ] ] ]
  
-           // Without Control.HasIconLeft fits in a phone screen
            div [ Style [ Display DisplayOptions.InlineFlex ] ] [ 
                         Field.div [ ]
                                   [ Label.label [ ] [ str "Account" ]
-                                    Control.div [ Control.HasIconLeft ]
+                                    Control.div [ ]
                                                 [ Select.select [ ]
                                                                 [ select [ DefaultValue "1" ]
                                                                          [ option [ Value "1"] [ str "Current" ]
@@ -160,20 +98,20 @@ let addExpensePage model dispatch =
 
                         Field.div [ ]
                                   [ Label.label [ ] [ str "Status" ]
-                                    Control.div [ Control.HasIconLeft ]
+                                    Control.div [ ]
                                                 [ Select.select [ ]
                                                                 [ select [ DefaultValue "1" ]
-                                                                         [ option [ Value "1"] [ str "Current" ]
-                                                                           option [ Value "2"] [ str "ISA" ]
-                                                                           option [ Value "3"] [ str "Wallet" ] ] ] ] ]
+                                                                         [ option [ Value "1"] [ str "New" ]
+                                                                           option [ Value "2"] [ str "Pending" ]
+                                                                           option [ Value "3"] [ str "Reconciled" ] ] ] ] ]
 
                         Field.div [ ]
                                   [ Label.label [ ] [ str "Type" ]
-                                    Control.div [ Control.HasIconLeft ]
+                                    Control.div [ ]
                                                 [ Select.select [ ]
                                                                 [ select [ DefaultValue "1" ]
-                                                                         [ option [ Value "1"] [ str "" ]
-                                                                           option [ Value "2"] [ str "" ]
+                                                                         [ option [ Value "1"] [ str "Regular" ]
+                                                                           option [ Value "2"] [ str "Internal transfer" ]
                                                                            option [ Value "3"] [ str "Scheduled" ] ] ] ] ]
            ]
 
@@ -207,7 +145,7 @@ let page model dispatch =
     | ShowExpenses
     | _ -> div [ ] [ str "Not implemented" ]
 
-let private view' model dispatch =
+let private view model dispatch =
     div [ ] [ Tabs.tabs [ Tabs.IsCentered ]
                         [ tab model dispatch AddExpense     "Add expense"
                           tab model dispatch ShowExpenses   "Show expenses" 
@@ -223,10 +161,7 @@ let private view' model dispatch =
                                         Card.Footer.a [ ]
                                                       [ str "Split" ] ] ] ]
 
-open Elmish.Debug
-open Elmish.HMR
-
-Program.mkProgram init update view'
+Program.mkProgram init update view
 |> Program.withReactSynchronous "elmish-app"
 #if DEBUG
 |> Program.withDebugger
