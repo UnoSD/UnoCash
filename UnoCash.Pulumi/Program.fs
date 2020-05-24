c903482b3081358e99c84020d08f13b8c9e624db
UnoSD <unosd.dev@gmail.com>
2020-05-24 15:10:13 +0100

    Pulumi infrastructure

---
+++ UnoCash.Pulumi/Program.fs
@@ -0,0 +1,23 @@
+﻿module Program
+
+open Pulumi.FSharp
+open Pulumi.Azure.Core
+open Pulumi.Azure.Storage
+
+let infra () =
+    let resourceGroup = ResourceGroup "UnoCash"
+
+    let storageAccount =
+        Account("unocash-storage",
+                AccountArgs(ResourceGroupName = io resourceGroup.Name,
+                            AccountReplicationType = input "LRS",
+                            AccountTier = input "Standard"))
+
+    dict [
+        ("ResourceGroupName", resourceGroup.Name :> obj)
+        ("StorageAccountName", storageAccount.Name :> obj)
+    ]
+
+[<EntryPoint>]
+let main _ =
+  Deployment.run infra
