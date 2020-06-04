4dbc8c70c70694abbde569ecff8a75f3969856d4
UnoSD <unosd.dev@gmail.com>
2020-06-04 08:56:25 +0100

    Create API management to back storage and functions

--- UnoCash.Pulumi/Program.fs
+++ UnoCash.Pulumi/Program.fs
@@ -1,6 +1,10 @@
 ﻿module Program
 
+open System
+open System.Runtime.CompilerServices
+open System.Threading.Tasks
 open Pulumi
+open Pulumi.Azure.ApiManagement
 open Pulumi.Azure.AppInsights
 open Pulumi.Azure.AppService
 open Pulumi.Azure.AppService.Inputs
@@ -95,18 +99,183 @@ let infra () =
                                                                        StringAsset :>
                                                                        AssetOrArchive))))
     
-    //let apiManagement =
-    //    Service("unocashapim",
-    //            ServiceArgs())
-    //let x =
-    //    CustomProviderAction()
-    //let cp =
-    //    CustomProvider("", CustomProviderArgs(Actions = inputList [ x ]))
+    let apiManagement =
+        Service("unocashapim",
+                ServiceArgs(ResourceGroupName = io resourceGroup.Name,
+                            SkuName = input "Consumption",
+                            PublisherName = input "UnoSD",
+                            PublisherEmail = input "info"))
+    
+    let webContainerUrl =
+        FormattableStringFactory.Create("https://{0}.blob.core.windows.net/$web", storageAccount.Name) |>
+        Output.Format
+    
+    let api =
+        Api("unocashapimapi",
+            ApiArgs(ResourceGroupName = io resourceGroup.Name,
+                    ApiManagementName = io apiManagement.Name,
+                    DisplayName = input "StaticWebsite",
+                    Name = input "staticwebsite",
+                    Path = input "",
+                    Protocols = inputList [ input "https"; input "http" ],
+                    Revision = input "1",
+                    ServiceUrl = io webContainerUrl))
+
+    let tokenToPolicy (taskTokenResult : Task<GetAccountBlobContainerSASResult>) =
+        sprintf """
+<policies>
+    <inbound>
+        <base />
+        <choose>
+            <when condition="@(context.Request.OriginalUrl.Scheme.ToLower() == "http")">
+                <return-response>
+                    <set-status code="303" reason="See Other" />
+                    <set-header name="Location" exists-action="override">
+                        <value>@("https://pizza.azure-api.net/" + context.Request.OriginalUrl.Path + context.Request.OriginalUrl.QueryString)</value>
+                    </set-header>
+                </return-response>
+            </when>
+        </choose>
+        <set-query-parameter name="sv" exists-action="override">
+            <value>2019-10-10</value>
+        </set-query-parameter>
+        <set-query-parameter name="ss" exists-action="override">
+            <value>b</value>
+        </set-query-parameter>
+        <set-query-parameter name="srt" exists-action="override">
+            <value>sco</value>
+        </set-query-parameter>
+        <set-query-parameter name="sp" exists-action="override">
+            <value>rx</value>
+        </set-query-parameter>
+        <set-query-parameter name="se" exists-action="override">
+            <value>%s</value>
+        </set-query-parameter>
+        <set-query-parameter name="st" exists-action="override">
+            <value>%s</value>
+        </set-query-parameter>
+        <set-query-parameter name="spr" exists-action="override">
+            <value>https</value>
+        </set-query-parameter>
+        <set-query-parameter name="sig" exists-action="override">
+            <value>%s</value>
+        </set-query-parameter>
+        <!--        <set-variable name="APIVersion" value="2012-02-12" />
+        <set-variable name="UTCNow" value="@(DateTime.UtcNow.ToString("R"))" />
+        <set-variable name="RequestPath" value="@(context.Request.Url.Path)" />
+        <set-header name="x-ms-date" exists-action="override">
+            <value>@(context.Variables.GetValueOrDefault<string>("UTCNow"))</value>
+        </set-header>
+        <set-header name="Authorization" exists-action="override">
+            <value>@{
+                    var account = "STORAGE ACCOUNT NAME";
+                    var key = "STORAGE ACCOUNT KEY";
+                    var splitPath = context.Variables.GetValueOrDefault<string>("RequestPath").Split('/');
+                    var container = "$web";
+                    var file = splitPath.Last();
+                    var dateToSign = context.Variables.GetValueOrDefault<string>("UTCNow");
+                    var stringToSign = string.Format("GET\n\n\n{0}\n/{1}/{2}/{3}", dateToSign, account, container, file);
+                    string signature;
+                    var unicodeKey = Convert.FromBase64String(key);
+                    using (var hmacSha256 = new HMACSHA256(unicodeKey))
+                    {
+                        var dataToHmac = Encoding.UTF8.GetBytes(stringToSign);
+                        signature = Convert.ToBase64String(hmacSha256.ComputeHash(dataToHmac));
+                    }
+                    var authorizationHeader = string.Format(
+                        "{0} {1}:{2}",
+                        "SharedKey",
+                        account,
+                        signature);
+                    return authorizationHeader;
+                }</value>
+        </set-header>-->
+        <rate-limit calls="100" renewal-period="300" />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+"""
+         (taskTokenResult.Result.Expiry)
+         (taskTokenResult.Result.Start)
+         (taskTokenResult.Result.Sas)
+        
+    let sasToken =
+        GetAccountBlobContainerSASArgs() |>
+        GetAccountBlobContainerSAS.InvokeAsync |>
+        (fun t -> t.ContinueWith(Func<Task<GetAccountBlobContainerSASResult>, string>(tokenToPolicy)))
+        
+    let x =
+        Func<string, Task<string>>(fun _ -> sasToken)
+        
+    let y =
+        Pulumi.InputExtensions.Apply<string, string>(input "", x)
+        
+    let _ =
+        ApiPolicy("unocashapimapipolicy",
+                  ApiPolicyArgs(ResourceGroupName = io resourceGroup.Name,
+                                ApiManagementName = io apiManagement.Name,
+                                ApiName = io api.Name,
+                                XmlContent = io y))
+        
+    let indexApiOperation =
+        ApiOperation("unocashapimindexoperation",
+                     ApiOperationArgs(ResourceGroupName = io resourceGroup.Name,
+                                      ApiManagementName = io apiManagement.Name,
+                                      ApiName = io api.Name,
+                                      UrlTemplate = input "/",
+                                      Method = input "GET",
+                                      DisplayName = input "GET index",
+                                      OperationId = input "get-index"))
+        
+    let _ =
+        ApiOperation("unocashapimoperation",
+                     ApiOperationArgs(ResourceGroupName = io resourceGroup.Name,
+                                      ApiManagementName = io apiManagement.Name,
+                                      ApiName = io api.Name,
+                                      UrlTemplate = input "/*",
+                                      Method = input "GET",
+                                      DisplayName = input "GET",
+                                      OperationId = input "get"))
+    
+    let indexPolicyXml = """
+<policies>
+    <inbound>
+        <base />
+        <rewrite-uri template="/index.html" />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+"""
+    
+    let _ =
+        ApiOperationPolicy("unocashapimiopolicy",
+                           ApiOperationPolicyArgs(ResourceGroupName = io resourceGroup.Name,
+                                                  ApiManagementName = io apiManagement.Name,
+                                                  ApiName = io api.Name,
+                                                  OperationId = io indexApiOperation.OperationId,
+                                                  XmlContent = input indexPolicyXml))
     
     dict [
         ("Hostname", app.DefaultHostname :> obj)
         ("StorageAccount", storageAccount.Name :> obj)
         ("SiteEndpoint", storageAccount.PrimaryWebEndpoint :> obj)
+        ("ApiManagementEndpoint", apiManagement.GatewayUrl :> obj)
     ]
 
 [<EntryPoint>]
