bb4da260c83d58f7893afb1f111fb22f09fbff68
UnoSD <unosd.dev@gmail.com>
2021-03-06 00:46:56 +0000

    Remove Fable project

--- UnoCash.Fulma/src/View.fs
+++
@@ -1,9 +0,0 @@
-module UnoCash.Fulma.View
-
-open UnoCash.Fulma.Tabs
-open UnoCash.Fulma.Pages
-open Feliz
-
-let view model dispatch =
-    Html.div [ tabs model dispatch
-               page model dispatch ]
